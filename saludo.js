5db5c71b1ec20ee0bc3b351e921f68d6fe8de543
monicrack <005.monica.gmail.com>
2025-10-01 09:25:09 +0200

    cambio para poder hacer pull request

--- saludo.js
+++ saludo.js
@@ -5,3 +5,4 @@ let apellido2 = " Aguaded";
 console.log("Hola "  + nombre, apellido1,apellido2 + "!");
 
 console.log("Voy a aprobar el módulo de DWEC");
+console.log("he añadido otro cambio por que en el pull request dice que no hay nada que comparar")
